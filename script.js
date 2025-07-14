7e5fc6ed0a8ec218e5a8a231385b0dbf5d246917
Raul Rodrigues <rm.rodrigues0307@gmail.com>
2025-07-14 11:11:12 -0300

    atualizando 2

---
+++ script.js
@@ -0,0 +1,13 @@
+var options = {
+ 
+    strings: [
+        'desenvolvedor Back-End', 
+        'estudante de SI', 
+        'entusiasta de Java e C#'
+    ],
+    typeSpeed: 50, 
+    backSpeed: 25, 
+    loop: true
+};
+
+var typed = new Typed('.maquina-escrever', options);
